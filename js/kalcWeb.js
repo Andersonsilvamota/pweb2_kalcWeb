5432564fa03ce7309dbbc7af9a2f56736cbbb403
Anderson Silva Mota <andersonsilv.mota@gmail.com>
2021-06-24 19:03:37 -0300

    Refatoração e erro de opereçoes com ponto corrigido

--- js/kalcWeb.js
+++ js/kalcWeb.js
@@ -1,82 +1,15 @@
-console.log("teste")
-
-// DOM
-
-// operaçoes
-const igual = document.getElementById("igual")
-const adicao = document.getElementById("adicao")
-const subtracao = document.getElementById("subtracao")
-const multiplicacao = document.getElementById("multiplicacao")
-const divisao = document.getElementById("divisao")
-
-// numeros
-const zero = document.getElementById("zero")
-const um = document.getElementById("um")
-const dois = document.getElementById("dois")
-const tres = document.getElementById("tres")
-const quatro = document.getElementById("quatro")
-const cinco = document.getElementById("cinco")
-const seis = document.getElementById("seis")
-const sete = document.getElementById("sete")
-const oito = document.getElementById("oito")
-const nove = document.getElementById("nove")
-const limpar = document.getElementById("limpar")
-const ponto = document.getElementById("ponto")
-
-// eventos
-
-igual.onclick = function(){
-    console.log(igual.innerText)
-}
-
-adicao.onclick = function(){
-    console.log(adicao.innerText)
-}
-subtracao.onclick = function(){
-    console.log(subtracao.innerText)
-}
-multiplicacao.onclick = function(){
-    console.log(multiplicacao.innerText)
-}
-divisao.onclick = function(){
-    console.log(divisao.innerText)
-}
-
-
-
-zero.onclick = function(){
-    console.log(zero.innerText)
-}
-um.onclick = function(){
-    console.log(um.innerText)
-}
-dois.onclick = function(){
-    console.log(dois.innerText)
-}
-tres.onclick = function(){
-    console.log(tres.innerText)
-}
-quatro.onclick = function(){
-    console.log(quatro.innerText)
-}
-cinco.onclick = function(){
-    console.log(cinco.innerText)
-}
-seis.onclick = function(){
-    console.log(seis.innerText)
-}
-sete.onclick = function(){
-    console.log(sete.innerText)
-}
-oito.onclick = function(){
-    console.log(oito.innerText)
-}
-nove.onclick = function(){
-    console.log(nove.innerText)
-}
-ponto.onclick = function(){
-    console.log(ponto.innerText)
-}
-limpar.onclick = function(){
-    console.log(limpar.innerText)
-}
+function insert(num)
+    {
+        var numero = document.getElementById('screen').innerHTML;
+        document.getElementById('screen').innerHTML = numero + num;
+    }
+function clean()
+    {
+        var resultado = document.getElementById('screen').innerHTML;
+        document.getElementById('screen').innerHTML = resultado.substring(0, resultado.length -1);
+    }
+function calcular()
+    {
+        var resultado = document.getElementById('screen').innerHTML;
+        document.getElementById('screen').innerHTML = eval(resultado);
+    }
